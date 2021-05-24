b88bbff2a57b28dfa670ae32b82df92a85312393
fabcodingzest <fabrizvi786@gmail.com>
2021-05-25 02:04:43 +0530

    tailwind setup done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ["./public/**/*.html", "./public/**/*.js"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
