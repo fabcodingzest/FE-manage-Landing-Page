2e516f4bd6f34707cfbb0774ad41eedbee415baa
fabcodingzest <fabrizvi786@gmail.com>
2021-05-31 22:51:16 +0530

    added mobile menu and made it work ha

---
+++ public/script.js
@@ -0,0 +1,15 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuOpen = document.querySelector(".hamburger");
+const menuClose = document.querySelector(".close-icon");
+
+menuOpen.addEventListener("click", () => {
+  console.log("open");
+  menuOpen.classList.add("hidden");
+  mobileMenu.classList.remove("hidden");
+});
+
+menuClose.addEventListener("click", () => {
+  console.log("close");
+  menuOpen.classList.remove("hidden");
+  mobileMenu.classList.add("hidden");
+});
