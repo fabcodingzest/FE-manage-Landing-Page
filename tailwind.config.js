3d8214418a73f94d58aff1ff8cd59af5fc807766
fabcodingzest <fabrizvi786@gmail.com>
2021-06-02 17:45:19 +0530

    ugh

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundPosition: {
+      "top-right-4": "top -4rem right -4rem",
+      "top-right-10": "top -10rem right -10rem",
+      "bottom-left-33": "top 33rem right -33rem",
+    },
     extend: {},
   },
   variants: {
