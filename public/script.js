524b51f5f1064da43cd7f3f6ce90744981740ed3
fabcodingzest <fabrizvi786@gmail.com>
2021-06-07 01:11:58 +0530

    added prev and next btns to testimonials

--- public/script.js
+++ public/script.js
@@ -3,6 +3,9 @@ const menuOpen = document.querySelector(".hamburger");
 const menuClose = document.querySelector(".close-icon");
 const indicatorBtns = document.querySelectorAll(".indicator-btn");
 const testimonialsContainer = document.querySelector(".testimonials-container");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
 const sliderObj = {
   0: "0",
   1: "-25",
@@ -10,13 +13,36 @@ const sliderObj = {
   3: "-75",
 };
 
+function changeSlide(i) {
+  testimonialsContainer.style.transform = `translateX(${sliderObj[i]}%)`;
+  indicatorBtns.forEach((item) => {
+    item.classList.remove("active");
+  });
+  indicatorBtns[i].classList.add("active");
+}
+
+function getKey() {
+  const matrix = window.getComputedStyle(testimonialsContainer).getPropertyValue("transform");
+  const width = parseInt(window.getComputedStyle(testimonialsContainer).getPropertyValue("width").split("").slice(0, -2).join(""));
+  let x = parseInt(matrix.match(/matrix.*\((.+)\)/)[1].split(", ")[4]);
+  let key = x < 0 ? ((x / width) * -100) / 25 : 0;
+  console.log(key);
+  return key;
+}
+
+prevBtn.addEventListener("click", () => {
+  let key = getKey() - 1;
+  if (key < 0) key = 3;
+  changeSlide(key);
+});
+nextBtn.addEventListener("click", () => {
+  let key = getKey() + 1;
+  if (key > 3) key = 0;
+  changeSlide(key);
+});
 indicatorBtns.forEach((btn, i) => {
   btn.addEventListener("click", () => {
-    testimonialsContainer.style.transform = `translateX(${sliderObj[i]}%)`;
-    indicatorBtns.forEach((item) => {
-      item.classList.remove("active");
-    });
-    indicatorBtns[i].classList.add("active");
+    changeSlide(i);
   });
 });
 
