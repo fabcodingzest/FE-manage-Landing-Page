cfaff2d3028d5bf2049516ede7f588b6bf13e8a2
fabcodingzest <fabrizvi786@gmail.com>
2021-06-07 04:04:08 +0530

    adding mouseevents and comments to javascript

--- public/script.js
+++ public/script.js
@@ -6,6 +6,7 @@ const testimonialsContainer = document.querySelector(".testimonials-container");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 
+// Object for transforating testimonials container
 const sliderObj = {
   0: "0",
   1: "-25",
@@ -13,6 +14,7 @@ const sliderObj = {
   3: "-75",
 };
 
+// changing container transform and indicator to active
 function changeSlide(i) {
   testimonialsContainer.style.transform = `translateX(${sliderObj[i]}%)`;
   indicatorBtns.forEach((item) => {
@@ -21,6 +23,7 @@ function changeSlide(i) {
   indicatorBtns[i].classList.add("active");
 }
 
+// getting the key for slider object
 function getKey() {
   const matrix = window.getComputedStyle(testimonialsContainer).getPropertyValue("transform");
   const width = parseInt(window.getComputedStyle(testimonialsContainer).getPropertyValue("width").split("").slice(0, -2).join(""));
@@ -29,28 +32,48 @@ function getKey() {
   return key;
 }
 
-setInterval(() => {
-  let key = getKey();
-  key = key + 1 > 3 ? 0 : key + 1;
-  changeSlide(key);
-}, 2000);
+let playSlider; // variable for storing interval
+const repeater = () => {
+  playSlider = setInterval(() => {
+    let key = getKey();
+    key = key + 1 > 3 ? 0 : key + 1;
+    changeSlide(key);
+  }, 2000);
+};
 
+repeater(); // calling the interval
+
+// stop auto play on mouseover by clearing interval
+testimonialsContainer.addEventListener("mouseover", () => {
+  clearInterval(playSlider);
+});
+
+// resume auto play on mouseout
+testimonialsContainer.addEventListener("mouseout", () => {
+  repeater();
+});
+
+// Changing slides with prev and next btn
 prevBtn.addEventListener("click", () => {
   let key = getKey() - 1;
   if (key < 0) key = 3;
   changeSlide(key);
 });
+
 nextBtn.addEventListener("click", () => {
   let key = getKey() + 1;
   if (key > 3) key = 0;
   changeSlide(key);
 });
+
+// Changing slides corresponding to indicators
 indicatorBtns.forEach((btn, i) => {
   btn.addEventListener("click", () => {
     changeSlide(i);
   });
 });
 
+//  Open & close mobile menu on click event
 menuOpen.addEventListener("click", () => {
   console.log("open");
   menuOpen.classList.add("hidden");
@@ -63,6 +86,7 @@ menuClose.addEventListener("click", () => {
   mobileMenu.classList.add("hidden");
 });
 
+// Close mobile menu if clicked outside of menu
 mobileMenu.addEventListener("click", (e) => {
   if (e.target == mobileMenu) {
     menuOpen.classList.remove("hidden");
