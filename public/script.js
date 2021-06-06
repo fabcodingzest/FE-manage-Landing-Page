e2c0144817d48a2075fcb8f06068721c9d3c1684
fabcodingzest <fabrizvi786@gmail.com>
2021-06-06 23:46:49 +0530

    section testimonials done

--- public/script.js
+++ public/script.js
@@ -1,6 +1,24 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuOpen = document.querySelector(".hamburger");
 const menuClose = document.querySelector(".close-icon");
+const indicatorBtns = document.querySelectorAll(".indicator-btn");
+const testimonialsContainer = document.querySelector(".testimonials-container");
+const sliderObj = {
+  0: "0",
+  1: "-25",
+  2: "-50",
+  3: "-75",
+};
+
+indicatorBtns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    testimonialsContainer.style.transform = `translateX(${sliderObj[i]}%)`;
+    indicatorBtns.forEach((item) => {
+      item.classList.remove("active");
+    });
+    indicatorBtns[i].classList.add("active");
+  });
+});
 
 menuOpen.addEventListener("click", () => {
   console.log("open");
